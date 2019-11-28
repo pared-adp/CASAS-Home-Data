dc9f2b519408775ed1702922e445a4e08a85e598
Angelina Paredes <33231863+pared-adp@users.noreply.github.com>
2019-11-27 20:40:38 -0800

    Add files via upload
    The code generates features from an annotated data set. I used a months worth of data starting May/30/2009 - June/30/2019

---
+++ activity_learner.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 21 20:35:05 2019
+
+@author: adpaw
+"""
+
+#preproccessing for features
+import pandas as pd
+import numpy as np
+import csv
+from datetime import datetime
+
+maxLength = 0
+temp = 0
+test = open('updated.csv', 'w')
+with open('month.txt', 'r') as f:
+    file_reader = csv.reader(f, delimiter='\t')
+    newLine = ''
+    for line in file_reader:
+        newLine = ",".join(str(bit) for bit in line) # add commas between words
+        test.write(newLine + '\n')
+        
+# creates a new column for time        
+#text = open("updated.csv", "r")
+#text = ''.join([i for i in text]).replace(" ", ",")
+#x = open("newUpdated.csv","w")
+#x.writelines(text)
+#x.close()                      
+test.close()
+            
+##DATA CLEANING##       
+data = pd.read_csv('updated.csv', names=["Date Time", "Sensor", "Message", "Activity", "Status"])
+data['Activity'].replace(np.nan, 'Standby', inplace=True)
+data['Status'].replace(np.nan, 'Waiting', inplace=True)
+
+win_indx = 0
+start = 0
+sensorNames = data.Sensor.unique()
+sensorNames = sensorNames + 'Window Length' + 'Time Difference' + 'Label'
+feature = pd.DataFrame(columns = sensorNames)
+window = 100
+date_str = '%Y-%m-%d %H:%M:%S.%f'
+#print(feature.head())
+diff = []
+last_event = []
+for row in range(0,len(data)):
+    if (row  + 1) % window == 0:
+        test = data.iloc[win_indx:row]
+        p1= test['Sensor'].value_counts() #sensor count features
+        feature = feature.append(p1, ignore_index = True)
+        
+        time1 = str(test['Date Time'].iat[0]) #converts to datetime structures to find difference
+        time2 = str(test['Date Time'].iat[-1])
+        d1 = datetime.strptime(time1, date_str)
+        d2 = datetime.strptime(time2, date_str)
+        diff.append(int((d2-d1) .total_seconds() / 60))
+        last_event.append(test.Activity.unique()[-1])
+        win_indx += window
+#generating additional feature values
+feature['Time Difference'] = np.array(diff)
+feature['Window Length'] = len(test)
+feature['Label'] = np.array(last_event)
+feature.replace(np.nan, 0, inplace = True)
+feature.to_csv('feature.csv') 
+print(feature.head(5))#window length feature       
+  
